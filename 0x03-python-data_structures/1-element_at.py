21573541e6552a21399a1719357b9651e635a265
emmabraboke <emmabraboke@gmail.com>
2022-06-06 21:35:44 +0100

    modified files

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if(idx<0):
+    if(idx < 0):
         return None
-    if(idx>len(my_list)-1):
+    if(idx > len(my_list) - 1):
         return None
     print("Element at index {:d} is {}".format(idx, my_list[idx]))
-
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,5 +8,4 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     new_list[idx] = element
 
-    print(new_list)
-    print(my_list)
+    return new_list
