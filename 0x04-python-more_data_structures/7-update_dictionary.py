19a7d3b0b6972637815d2b36f829b277e2f681a0
emmabraboke <emmabraboke@gmail.com>
2022-06-08 22:08:14 +0100

    modified file

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if key != str:
+        pass
+    else:
+        a_dictionary[key] = value
     return a_dictionary
     
