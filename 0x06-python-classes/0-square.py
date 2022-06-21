2c24255ef1996a3e9773ab9c5406c6a2147f1347
emmabraboke <emmabraboke@gmail.com>
2022-06-21 16:42:23 +0000

    add documentation to file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-class Square:
-    """Square
-    Square,in geometry, a plane figure with four
-    equal sides and four right (90°) angles.
-    A square is a special kind of rectangle (an equilateral one)
-    A square has four axes of symmetry, and its
-    two finite diagonals (as with any rectangle) are equal.
-    Bisection of a square by a diagonal results in two right triangles.
-    If the length of the side of a square is s, then
-    the area of the square is s2, or “s squared.”
 
-    """
+"Empty class"
+
+
+class Square:
+    """ Empty class """
     pass
