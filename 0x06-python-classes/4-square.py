bc06c3e01b9fda8993a40e2b730721ca392ea5c1
emmabraboke <emmabraboke@gmail.com>
2022-06-21 20:42:37 +0100

    modify file

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,7 +14,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     """ getters """
     @property
